788f444a9516ae69877de53cf4f632d24d3c8abd
Francisco Duarte M <frduartemartinez@davidson.edu>
2022-09-17 18:55:19 -0400

    Setup

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,14 @@ Staaatliches Bauhaus (1919-1933)
 `;
 
 console.log(bauhaus);
+// while(event.keyCode == 16){
+//   console.log(event.ClientX,event.ClientY)
+// }
+
+window.addEventListener("keydown",function(event){
+  if(event.key = "n"){
+    console.log("X position: " + event.screenX + " Y position: " + event.screenY);
+  }
+
+  event.preventDefault();
+},false);
