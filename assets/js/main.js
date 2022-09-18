497b6544beca9d1e82708dff2eb470cbfb4b4c0d
Francisco Duarte M <frduartemartinez@davidson.edu>
2022-09-18 00:19:14 -0400

    Fixed bug
    Element total did not show up because js was loaded before it was called. Adding async makes it still work while being in the head

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,7 @@ document.addEventListener("mousemove", () => {
 
 let totalImgElem = document.querySelectorAll('.content')[0].querySelectorAll("div").length;
 console.log("There are a total of " + totalImgElem + " elements used in the creation of this image");
-document.getElementById("elem").innerHTML = "There is a total of " + totalImgElem +
-  " elements in use to create this image";
+
+document.getElementById("elem").innerHTML = "There is a total of " + totalImgElem +" elements in use to create this image";
 
 document.body.style.zoom = "80%";
